003bbb9db1656913009ad3ba97f12b1496b79a57
Oscar Silva <oscarsilva2356@gmail.com>
2022-05-10 17:27:38 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            // employees = PeopleFetcher.GetEmployees(); 
+            Util.PrintEmployees(employees);
+            Util.MakeCSV(employees);
+            Util.MakeBadges(employees);
+            employees = PeopleFetcher.GetFromApi();
+        }
+    }
+}
